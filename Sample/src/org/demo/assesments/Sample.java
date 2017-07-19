36a32c13440f90136655187d13669c6cfa88e235
rans <rans@tests-air>
2017-07-18 22:47:26 -0400

    initial checkin of sample class

---
+++ Sample/src/org/demo/assesments/Sample.java
@@ -0,0 +1,18 @@
+package org.demo.assesments;
+
+public class Sample {
+	
+	
+	public static void main(String args[]){ 
+
+		Sample test = new Sample();
+		test.displayMessage();
+		
+		//public static void main(String[] args){ 
+}
+	
+	public void displayMessage() {
+		System.out.println("Hello World");
+	}
+	
+}
